3dd7c87142600556495edec7428d3d7329873293
Allana Gross <jennifergross@Jennifers-MacBook-Air.local>
2021-01-20 19:22:47 -0600

    recursion yay

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -5,5 +5,39 @@
 // But instead we're going to implement it from scratch:
 
 const getElementsByClassName = (className) => {
-  // TODO: Your code here!
+  //input: string 
+  //output: array 
+    //create a variable to return and store results in 
+    let results = [];
+
+    //we need to access the document body element so assign a variable to it 
+    let docB = document.body; 
+
+    
+    //create a function to check for classes in the document body 
+    let classChecker = function (docB){
+      //use a conditional to check for class name 
+      if(docB.classList && docB.classList.contains(className)){
+        //push the results of what you find into the results array 
+        results.push(docB)
+      }
+      
+      //look to see if any of the elements within the body have child nodes associated with the class attributes
+      if(docB.hasChildNodes()){
+        //we are going to create a for loop to loop through the data of child node attributes in the document and call the classChecker on them 
+        for(let i = 0; i < docB.childNodes.length; i++){
+          classChecker(docB.childNodes[i]);
+        }
+      }
+    }
+    //recursive call on the function you just created to check for classes 
+    //use recursive call to keep the function going until it hits a breaking point 
+    classChecker(docB)
+
+    //return the results of running your function 
+    return results; 
+
+
+   
+ 
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,5 +3,35 @@
 // But you don't so you're going to write it from scratch:
 
 const stringifyJSON = (obj) => {
-  // TODO: Your code here!
+//check the value that is being passed in and determine if it is an array or object 
+if( Array.isArray(obj) ){
+  //create a variabl to return with the results 
+  var results = [];
+  //loop through the object and push the elements into the results variable and return the values as a string 
+  for( var i = 0; i < obj.length; i++ ){
+    results.push( stringifyJSON(obj[i]) );
+  }
+  return '[' + results.join(',') + ']';
+}
+//test if the object is an object and repeat the steps from above 
+if( obj && typeof obj === 'object' ){
+  //create an empty variable to store results 
+  var results = [];
+  //loop through the object and if it passes the conditional continue to push the results into the results array and be sure to return it as a string 
+  for( var key in obj ){
+    if( obj[key] === undefined || typeof obj[key] === 'function'){
+      continue;
+    }
+    results.push(
+      //use stringify to turn the values passed in into strings 
+      stringifyJSON(key) + ':' + stringifyJSON(obj[key])
+    );
+  }
+  return '{' + results.join(',') + '}';
+}
+//test if the element passed in is a string 
+if( typeof obj === 'string' ){
+  return '"'+obj+'"';
+}
+return ''+ obj;
 };
